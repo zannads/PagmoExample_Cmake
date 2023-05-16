452670753f67a2429c78966368ac1cf56ec6cb06
Dennis <65568528+zannads@users.noreply.github.com>
2023-05-16 11:14:03 +0200

    Add project files.

---
+++ PagmoExample_Cmake/PagmoExample_Cmake.cpp
@@ -0,0 +1,59 @@
+﻿// PagmoExample_Cmake.cpp : Defines the entry point for the application.
+//
+
+#include "PagmoExample_Cmake.h"
+
+using namespace pagmo;
+
+// Our simple example problem, version 0.
+struct problem_v1 {
+    // Number of equality constraint 
+    vector_double::size_type get_nec() const {
+        return  1;
+    }
+    // number of inequality constraint
+    vector_double::size_type get_nic() const {
+        return 1;
+    }
+
+
+    // Implementation of the objective function.
+    vector_double fitness(const vector_double& dv) const
+    {
+        return {
+            dv[0] * dv[3] * (dv[0] + dv[1] + dv[2]) + dv[2], //obj fun 
+            dv[0] * dv[0] + dv[1] * dv[1] + dv[2] * dv[2] + dv[3] * dv[3] - 40., // equality con.
+            25. - dv[0] * dv[1] * dv[2] * dv[3]                                  // inequality con.
+        };
+    }
+    // Implementation of the box bounds.
+    std::pair<vector_double, vector_double> get_bounds() const
+    {
+        return { {1., 1., 1., 1.}, {5., 5., 5., 5.} };
+    }
+};
+
+int main()
+{
+    // Construct a pagmo::problem from our example problem.
+    problem p{ dtlz(1, 10, 2) };
+
+    // Compute the value of the objective function
+    // in the point (1, 2, 3, 4).
+   // std::cout << "Value of the objfun in (1, 2, 3, 4): " << p.fitness({ 1, 2, 3, 4 })[0] << '\n';
+
+    //compute fitness: obj func + eq + ineq 
+
+
+    //p.set_seed(3);
+    // 2 - Instantiate a pagmo algorithm
+    algorithm algo{ nsga2(100) };
+    algo.set_seed(3);
+
+    population pop{ p, 24, 3 };
+
+    pop = algo.evolve(pop);
+
+    // Print p to screen.
+    std::cout << pop << '\n';
+}
---
+++ PagmoExample_Cmake/PagmoExample_Cmake.h
@@ -0,0 +1,16 @@
+﻿// PagmoExample_Cmake.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <cmath>
+#include <initializer_list>
+#include <iostream>
+#include <utility>
+
+#include <pagmo/problem.hpp>
+#include <pagmo/problems/dtlz.hpp>
+#include <pagmo/types.hpp>
+#include <pagmo/population.hpp>
+#include <pagmo/algorithm.hpp>
+#include <pagmo/algorithms/nsga2.hpp>
